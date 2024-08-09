466b8b7247faa176aad17af89209c4b1b91e9b84
Devank Khuteta <devankkh123@gmail.com>
2024-08-09 22:33:24 +0530

    video changes

---
+++ FestavaLive-1.0.0/js/video.js
@@ -0,0 +1,18 @@
+// Get the video element
+const video = document.getElementById('video');
+
+// Add an event listener to the video element to detect when it ends
+video.addEventListener('ended', function() {
+  console.log('Video ended');
+  
+  // Get the next section element
+  const nextSection = document.getElementById('section_2');
+  
+  console.log('Next section:', nextSection);
+  
+  // Scroll to the next section
+  window.scrollTo({
+    top: nextSection.offsetTop,
+    behavior: 'smooth'
+  });
+});
